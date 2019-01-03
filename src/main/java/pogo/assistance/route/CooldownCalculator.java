d3ae8a3216cd131250961669c76e36896e3f51ad
Hasan Imam <miimam@amazon.com>
2019-01-03 18:44:16 +0600

    Update cooldown mapping
    Mappings are now more granular. Plus, some mappings seems incorrect
    based on this latest mapping.

--- src/main/java/pogo/assistance/route/CooldownCalculator.java
+++ src/main/java/pogo/assistance/route/CooldownCalculator.java
@@ -26,22 +26,62 @@ public class CooldownCalculator {
 
     static {
         final NavigableMap<Double, Double> kmToSecondCd = new TreeMap<>();
-        // Source: https://twitter.com/stardustpokmngo/status/873882137446957056
-        kmToSecondCd.put(0D, 0D);
-        kmToSecondCd.put(0.5D, 0D);
-        kmToSecondCd.put(1D, 30D);
-        kmToSecondCd.put(5D, 2 * 60D);
-        kmToSecondCd.put(10D, 6 * 60D);
-        kmToSecondCd.put(25D, 11 * 60D);
-        kmToSecondCd.put(30D, 14 * 60D);
-        kmToSecondCd.put(65D, 22 * 60D);
-        kmToSecondCd.put(81D, 25 * 60D);
-        kmToSecondCd.put(100D, 35 * 60D);
-        kmToSecondCd.put(250D, 45 * 60D);
-        kmToSecondCd.put(500D, 60 * 60D);
-        kmToSecondCd.put(750D, 1.25 * 60 * 60D);
-        kmToSecondCd.put(1000D, 1.50 * 60 * 60D);
+        kmToSecondCd.put(0.5D,   0 * 60D);
+        kmToSecondCd.put(1D,   0.1 * 60D);
+        kmToSecondCd.put(2D,   1.5 * 60D);
+        kmToSecondCd.put(3D,   2.5 * 60D);
+        kmToSecondCd.put(4D,     3 * 60D);
+        kmToSecondCd.put(5.5D,   4 * 60D);
+        kmToSecondCd.put(6D,   4.5 * 60D);
+        kmToSecondCd.put(7D,     5 * 60D);
+        kmToSecondCd.put(10D,    6 * 60D);
+        kmToSecondCd.put(11D,    7 * 60D);
+        kmToSecondCd.put(12.7D,  8 * 60D);
+        kmToSecondCd.put(15D,    9 * 60D);
+        kmToSecondCd.put(18D,   10 * 60D);
+        kmToSecondCd.put(20.5D, 11 * 60D);
+        kmToSecondCd.put(22D,   13 * 60D);
+        kmToSecondCd.put(24D,   14 * 60D);
+        kmToSecondCd.put(26D,   15 * 60D);
+        kmToSecondCd.put(28D,   16 * 60D);
+        kmToSecondCd.put(30.5D, 17 * 60D);
+        kmToSecondCd.put(40D,   18 * 60D);
+        kmToSecondCd.put(42D,   19 * 60D);
+        kmToSecondCd.put(53D,   21 * 60D);
+        kmToSecondCd.put(73D,   22 * 60D);
+        kmToSecondCd.put(78D,   23 * 60D);
+        kmToSecondCd.put(88.5D, 24 * 60D);
+        kmToSecondCd.put(93D,   26 * 60D);
+        kmToSecondCd.put(105D,  27 * 60D);
+        kmToSecondCd.put(113D,  28 * 60D);
+        kmToSecondCd.put(133D,  31 * 60D);
+        /*
+         * Following numbers are odd because the original numbers had "less-than" condition. For example:
+         *     Original mapping condition: "< 500 KM -> 62 mins cooldown"
+         *     Map entry: key: 499, value: 62 * 60
+         * This is necessary because we lookup cooldown get querying ceiling entry in this navigable map
+         */
+        kmToSecondCd.put(499D,  62 * 60D);
+        kmToSecondCd.put(549D,  66 * 60D);
+        kmToSecondCd.put(599D,  70 * 60D);
+        kmToSecondCd.put(649D,  74 * 60D);
+        kmToSecondCd.put(699D,  77 * 60D);
+        kmToSecondCd.put(750D,  82 * 60D);
+        kmToSecondCd.put(801D,  84 * 60D);
+        kmToSecondCd.put(838D,  88 * 60D);
+        kmToSecondCd.put(898D,  90 * 60D);
+        kmToSecondCd.put(899D,  91 * 60D);
+        kmToSecondCd.put(947D,  95 * 60D);
+        kmToSecondCd.put(1006D, 98 * 60D);
+        kmToSecondCd.put(1019D, 102 * 60D);
+        kmToSecondCd.put(1099D, 104 * 60D);
+        kmToSecondCd.put(1179D, 109 * 60D);
+        kmToSecondCd.put(1199D, 111 * 60D);
+        kmToSecondCd.put(1220D, 113 * 60D);
+        kmToSecondCd.put(1299D, 117 * 60D);
+        kmToSecondCd.put(1343D, 119 * 60D);
         kmToSecondCd.put(1500D, 2 * 60 * 60D);
+        kmToSecondCd.put(Double.MAX_VALUE, 2 * 60 * 60D);
         KM_TO_SECOND_CD_TABLE = Collections.unmodifiableNavigableMap(kmToSecondCd);
     }
 
